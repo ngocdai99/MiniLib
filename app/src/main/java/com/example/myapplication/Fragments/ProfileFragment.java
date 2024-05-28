aa4c32cbbb3b3ae1ae328180ec143ff535a1b00a
BotFarHome <magic.from.zion@gmail.com>
2024-05-28 11:43:12 +0700

    add fragment home, order, profile layouts, init their java files

---
+++ app/src/main/java/com/example/myapplication/Fragments/ProfileFragment.java
@@ -0,0 +1,24 @@
+package com.example.myapplication.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.databinding.FragmentOrderBinding;
+import com.example.myapplication.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+    FragmentProfileBinding binding;
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
